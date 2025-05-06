2c82fc5fbf9053efc22cb55b09f4d83eba40fdd6
Santosh-7124 <sunny962002@gmail.com>
2025-05-06 16:13:03 +0530

    Uodates

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Navbar from "./Navbar";
+import Whatsapp from "./Whatsapp";
 import Footer from "./Footer";
 import WebsiteDesigning from "./WebsiteDesigning";
 
@@ -9,6 +10,7 @@ function Main() {
   return (
     <>
       <Navbar />
+      <Whatsapp />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/web_desgining" element={<WebsiteDesigning />} />
