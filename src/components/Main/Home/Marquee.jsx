47e8dbf1eceae38a9293b88455ebe060e81265c0
Santosh-7124 <sunny962002@gmail.com>
2025-05-23 12:19:39 +0530

    Fix

--- src/components/Main/Home/Marquee.jsx
+++ src/components/Main/Home/Marquee.jsx
@@ -39,7 +39,7 @@ function Marquee() {
             engaging experiences.
           </p>
         </section>
-        <div class="p-mail-button">
+        <div className="p-mail-button">
           <p>Explore Now</p>
           <svg
             xmlns="http://www.w3.org/2000/svg"
