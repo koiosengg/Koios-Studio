f83ce06145ff9885e958832ebcab0231430097cd
Panth1823 <panthshah242@gmail.com>
2026-09-07 17:55:33 +0530

    deleted payal & priyanka mylynk

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,9 @@ const Main = lazy(() => import("./components/Main/Main"));
 const Portfolio = lazy(() => import("./components/Portfolio/Portfolio"));
 const TemplateTKS = lazy(() => import("./components/Virtual Card/TemplateTKS"));
 const MudigetiSantosh = lazy(() => import("./components/Virtual Card/MudigetiSantosh"));
-const PriyankaKP = lazy(() => import("./components/Virtual Card/PriyankaKP"));
 const LakshitJain = lazy(() => import("./components/Virtual Card/LakshitJain"));
 const ShvethaS = lazy(() => import("./components/Virtual Card/ShvethaS"));
 const AbhayNayak = lazy(() => import("./components/Virtual Card/AbhayNayak"));
-const Payal = lazy(() => import("./components/Virtual Card/Payal"));
 const MahiRastogi = lazy(() => import("./components/Virtual Card/MahiRastogi"));
 const TemplateKES = lazy(() => import("./components/Virtual Card/TemplateKES"));
 const BalasubramanyamM = lazy(() => import("./components/Virtual Card/BalasubramanyamM"));
@@ -31,11 +29,9 @@ function App() {
             <Route path="portfolio/*" element={<Portfolio />} />
             <Route path="TemplateTKS" element={<TemplateTKS />} />
             <Route path="MudigetiSantosh" element={<MudigetiSantosh />} />
-            <Route path="PriyankaKP" element={<PriyankaKP />} />
             <Route path="LakshitJain" element={<LakshitJain />} />
             <Route path="ShvethaS" element={<ShvethaS />} />
             <Route path="AbhayNayak" element={<AbhayNayak />} />
-            <Route path="Payal" element={<Payal />} />
             <Route path="MahiRastogi" element={<MahiRastogi />} />
             <Route path="TemplateKES" element={<TemplateKES />} />
             <Route path="BalasubramanyamM" element={<BalasubramanyamM />} />
